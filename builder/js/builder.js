82a09af06afd7fb6536336bedebbec24471943cb
chris jones <cjones@cbnco.com>
2015-10-05 17:21:05 -0400

    some changes to builder.js

--- builder/js/builder.js
+++ builder/js/builder.js
@@ -3,7 +3,7 @@ var html = '';
 
 html+='<p>';
 
-html += "# name item_count building/s buildin_next_level/s</p><p>";
+html += "# name item_count accumulate/s building/s</p><p>";
 
 for (var i=0; i < items_arr.length; i++) {
     html +=  i+". " + items_arr[i]+'&nbsp;';
@@ -13,8 +13,8 @@ for (var i=0; i < items_arr.length; i++) {
     html += "<input type=text value='0/s' id='" + items_arr[i] + "_build_rate' size=15>";
 
     for (var j=0; j < levels_per_item; j++) {
-		html += "<input type=\"button\" onclick=\"build( '" + items_arr[i] + "', "+ j +" );\" value=\"" + calc(j) +"\">";
-
+        if (i==0 && j==0 || i>0) // hide all add buttons for item[0]
+            html += "<input type=\"button\" onclick=\"build( '" + items_arr[i] + "', "+ j +" );\" value=\"" + calc(j) +"\">";        
 	}
 
 	for (var j=0; j < levels_per_item; j++) {

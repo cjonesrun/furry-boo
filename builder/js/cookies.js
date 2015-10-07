1ab730da0aa2f07429ff6ed5746bfeced0e52b2b
chris jones <cjonesrun@gmail.com>
2015-10-07 00:59:56 -0400

    update

--- builder/js/cookies.js
+++ builder/js/cookies.js
@@ -54,13 +54,13 @@ function initFromCookies() {
 
 function saveState()
 {
-    console.log("cookie item count = ", item_count_map);
-    console.log("cookie item rate = ", rate_map );
+    /*console.log("cookie item count = ", item_count_map);
+    console.log("cookie item rate = ", rate_map );*/
 
 	document.setCookie("item_count",JSON.stringify(item_count_map));
     document.setCookie("rate_count",JSON.stringify(rate_map));
 
-	console.log("item_count = ", document.getCookie("item_count"));
-	console.log("rate_count = ", document.getCookie("rate_count"));
+	/*console.log("item_count = ", document.getCookie("item_count"));
+	console.log("rate_count = ", document.getCookie("rate_count"));*/
 
 }
